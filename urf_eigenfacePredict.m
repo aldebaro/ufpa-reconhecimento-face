bb3fc0727f53c8be705442ff1baa2cad3b86b615
Lidia Gianne <lidia_pokemon@yahoo.com.br>
2016-03-03 20:22:50 -0300

    Delete urf_eigenfacePredict.m

--- urf_eigenfacePredict.m
+++
@@ -1,9 +0,0 @@
-function C = urf_eigenfacePredict (model, Xtest, k)
-
-	Q = model.W' * (Xtest - model.mu);  %multiplica os autovetores pela matriz com as eigenfaces
-	C = knn(model.P, model.y, Q, k);    %chama a função knn usando como parâmetros:
-                                            %model.P = matriz das imagens de treino do banco de dados 
-                                            %model.y= número de imagens na matriz P
-                                            %Q = resultado da multiplicação acima
-                                            %k = número de imagens a serem analisadas
-end
