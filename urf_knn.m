7f2c106c4bd5dbedabe13e9f48413fe6b6115074
Lidia Gianne <lidia_pokemon@yahoo.com.br>
2016-03-03 19:45:00 -0300

    explicado

--- urf_knn.m
+++ urf_knn.m
@@ -1,3 +1,11 @@
 function c = knn(P, y, Q,  k)
-   
+%avisa qual a coluna da imagem que está mais perto da que está sendo analisada
+%entra com: 
+%        P = matriz das imagens de treino que estao no banco de dados
+%        y = numero de imagens que tem em P
+%        Q = vetor imagem de teste
+%        k = numero de imagens a ser analisadas
+%sai com:
+%       c = posiçao da coluna na matriz que tem mais semelhanças com a imagem sendo analisada
+
 end
