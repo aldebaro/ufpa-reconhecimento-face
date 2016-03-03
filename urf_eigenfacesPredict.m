cb0c11342ce94c01730e3685db873aba8601fc3a
Lidia Gianne <lidia_pokemon@yahoo.com.br>
2016-03-03 19:39:02 -0300

    explicado

--- urf_eigenfacesPredict.m
+++ urf_eigenfacesPredict.m
@@ -1,3 +1,10 @@
 function C = eigenfaces_predict(model, Xtest, k)
+%avisa qual a coluna da imagem que está mais perto da que está sendo analisada
+%entra com: 
+%       model = onde tem as imagens de treino e suas "estruturas"
+%       xtest = vetor da imagem que está sendo analisada
+%       k = numero de imagens a ser analisadas
+%sai com:
+%       c = posiçao da coluna na matriz que tem mais semelhanças com a imagem sendo analisada
 
 end
