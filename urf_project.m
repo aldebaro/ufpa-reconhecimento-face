9cfafc14da9e91e950fa75ff937972b23b6fc407
Lidia Gianne <lidia_pokemon@yahoo.com.br>
2016-03-03 20:51:07 -0300

    Update urf_project.m

--- urf_project.m
+++ urf_project.m
@@ -1,4 +1,4 @@
-function Y = project(X, W, mu)
+function Y = urf_project(X, W, mu)
 %ajeita a imagem em estilo pca, so que somente para uma imagem, por isso trabalha em matriz
 %entra com: 
 %       X = matriz da imagema ser analisada
