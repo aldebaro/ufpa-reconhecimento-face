f7d0066546b8c089b70fc28c891cf7b64f470f9f
Helderanr <helderanr@gmail.com>
2016-03-03 20:43:01 -0300

    Added files via upload

--- lbp.m
+++ lbp.m
@@ -1,55 +1,59 @@
 % Copyright (c) Philipp Wagner. All rights reserved.
 % Licensed under the BSD license. See LICENSE file in the project root for full license information.
 
-function L = lbp(X, radius, neighbors) 
-	%% Description:
-	%%	Calculates the Extended Local Binary Patterns of X with given radius 
-	%%	and neighbors. This is a slightly modified version of: 
-	%%	http://www.cse.oulu.fi/CMV/Downloads/LBPMatlab by authors Marko Heikkilä 
-	%%	and Timo Ahonen.
+function L = lbp(X, raio, vizinhos) 
+	%% Descrição:
+	%%	Calcula o padrões binários locais estendidos de X dado raio e sua vizinhança;	
 	%%
-	%% Arguments:
-	%%	radius [int] 
-	%%	neighbors [int]
-	%%
-	%% Example:
-	%%	I=imread("/path/to/image.jpg");
-	%%	G=rgb2gray(I);
-	%%	L=lbp(G);
-	%%	imshow(uint8(L))
+	%% Argumentos:
+	%%	raio [int] 
+	%%	vizinhos [int]
 	%%
 	X = double(X);
 	% get origin of a block
-	origy = radius+1;
-	origx = radius+1;
+	% adquirir origem do bloco	
+	origy = raio+1;
+	origx = raio+1;
 	% blocks to process
-	dy = size(X,1)-(2*radius+1);
-	dx = size(X,2)-(2*radius+1);
+	% Blocos para processamento
+	dy = size(X,1)-(2*raio+1);
+	dx = size(X,2)-(2*raio+1);
 	% create result matrix
+	% Criação da matriz resultante
 	L = zeros(dy+1,dx+1);
 	% get center matrix
+	% Adquirir o centro da Matriz
 	C = X(origy:origy+dy,origx:origx+dx);
 	% iterate through circle
-	for n=1:neighbors
+	% iteração no circulo
+	for n=1:vizinhos
 		% sample points
-		y = radius * -sin(2*pi*((n-1)/neighbors)) + origy;
-		x = radius * cos(2*pi*((n-1)/neighbors)) + origx;
+		% Pontos de Amostra
+		y = raio * -sin(2*pi*((n-1)/vizinhos)) + origy;
+		x = raio * cos(2*pi*((n-1)/vizinhos)) + origx;
 		% relative indices
+		% Indices Relativos
+		% Valores imediatamente menores
 		fx = floor(x);
 		fy = floor(y);
+		% Valores imediatamente maiores
 		cx = ceil(x);
 		cy = ceil(y);
 		% fractional parts
+		% Calcula as partes fracionárias, subtranindo do numero original o seu numero imediatamente menor.
 		tx = x - fx;
 		ty = y - fy;
 		% interpolation weights
+		% Calculando pesos de interpolação
 		w1 = (1 - tx) * (1 - ty);
 		w2 =      tx  * (1 - ty);
 		w3 = (1 - tx) *      ty ;
 		w4 =      tx  *      ty ;
 		% get interpolated image
+		% Adquirindo a imagem interpolada, multiplicando o peso de interpolação pela matriz em seus blocos
 		N = w1*X(fy:fy+dy,fx:fx+dx) + w2*X(fy:fy+dy,cx:cx+dx) + w3*X(cy:cy+dy,fx:fx+dx) + w4*X(cy:cy+dy,cx:cx+dx);
 		% calculate binary value for current neighbor, update result
+		% Calcula o valor binario refente a vizinhaça correspondente e atualiza o resultado.
 		L += (N>=C)*(2^(n-1));
 	end
 end
