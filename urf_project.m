de8291cd329cda1fb772cc9345f11c7729abad5a
Lidia Gianne <lidia_pokemon@yahoo.com.br>
2016-03-03 20:01:02 -0300

    explicado

--- urf_project.m
+++ urf_project.m
@@ -1,3 +1,10 @@
-function model = lda(X, y, num_components)
+function Y = project(X, W, mu)
+%
+%entra com: 
+%       X = matriz da imagema ser analisada
+%       W = autovalores das imagens de treino
+%       mu = media entre as imagens de treino
+%sai com:
+%       Y = projeção (matriz quase pronta (so falta virar coluna) para ser usada como imagem de treino)
 
 end
