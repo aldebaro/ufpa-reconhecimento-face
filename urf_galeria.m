4a0a9d33fe10699473bad8582af12dfc62394c03
paulo Alberto <paulo.albertklik@gmail.com>
2016-03-03 16:12:24 -0300

    adicionando comentarios em português para a função galeria

--- urf_galeria.m
+++ urf_galeria.m
@@ -1,2 +1,14 @@
 %baseado em gallery.m
+%editado por albertklik
+
 function urf_galeria(caminho, nome_arquivo, linhas, colunas)
+% cria uma galeria a partir de um diretorio de imagens dado
+% Argumentos:
+%  caminho - endereço do diretório onde as imagens estão localizadas 
+%            para a criação da galeria.
+%
+%  nome_arquivo - Nome d
+
+
+% cria um vetor com os arquivos do diretorio e atribui a variavel "imagens" 
+imagens = urf_listarArquivos(caminho);
