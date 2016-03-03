fe3ff5aa4da194fa97702c80898636151fcb7809
DeivisonMacone <profissionalmarcone@gmail.com>
2016-03-03 14:36:07 -0300

    Update urf_batch_crop.m

--- urf_batch_crop.m
+++ urf_batch_crop.m
@@ -1,1 +1,10 @@
-#colocar alguma coisa aqui
+function N = normalize(I, l, h)
+    minI = min(I);
+    maxI = max(I);
+    %% normalize between [0...1]
+    N = I - minI;
+    N = N ./ (maxI - minI);
+    %% scale between [l...h]
+    N = N .* (h-l);
+    N = N + l;
+end
