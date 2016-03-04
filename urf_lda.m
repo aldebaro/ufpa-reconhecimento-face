13853d3e95f8499f2245478bc0fd167cccac8c2e
Lidia Gianne <lidia_pokemon@yahoo.com.br>
2016-03-03 21:10:43 -0300

    Update urf_lda.m

--- urf_lda.m
+++ urf_lda.m
@@ -1,3 +1,15 @@
 function model = urf_lda(X, y, num_components)
+%executa uma analise linear e retorna o numero de componentes
+%aleatorio, mas descrescendo pelos autovalores
+%
+%entra com:
+%       X = matriz de uma imagem de teste
+%       y = numero de imagens que tem em X
+%       num_components = numero de componentes para manter
+%sai com:
+%uma estrutura 'model' com os seguites atributos
+%      .name = nome do modelo (urf_lda)
+%      .num_components = numero de componentes no modelo
+%      .W = componentes identificados pelo urf_lda
 
 end
