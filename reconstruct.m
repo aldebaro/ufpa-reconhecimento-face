465dbfda62a414aed17e11ee944bda672ace7661
felipebastosb <felipe.bastos@ymail.com>
2016-03-04 08:42:43 -0300

    Update reconstruct.m

--- reconstruct.m
+++ reconstruct.m
@@ -1,10 +1,10 @@
-funcao X = reconstrucao(W, Y, mu)
+funcao X = reconstrucao(P, M, ms)
     %%  Recupera Y de X usando W,  mu pode ajudar a ajustar a média.
     %%
     %%  Argumentos:
-    %%    C [Número de componentes x Número de informações] projeção
-    %%    D [Dimenção x Número de componentes] matriz transformada
-    %%    mu [Dimenção x 1] média simples (opicional) (Somatoria de até N dividido por N)
+    %%    P [Número de componentes x Número de informações] projeção
+    %%    M [Dimenção x Número de componentes] matriz transformada
+    %%    ms [Dimenção x 1] média simples (opicional) (Somatoria de até N dividido por N)
     %%
     %%  Retornos:
     %%    X [Dimenção x Núemro de informações] Informação reconstruida
@@ -13,10 +13,10 @@ funcao X = reconstrucao(W, Y, mu)
     switch nargin     %% Verifica Quantos argumentos foram passados para a função
     case 2            %% Caso apenas dois argumentos tenham sido passados (A projeção e a matriz transformada
                       %% Multiplica a projeção com a matriz para se recuperar a informação (face)
-         X = W * Y;   
+         X = P * M;   
     case 3            %% Caso três argumentos argumentos tenham entrado, a projeção, a matriz e mais a média simples
                       %% Recupera a informação como no caso anterior e soma as informações que mais se repetem (mais marcantes)
-        X = W*Y +repmat(mu,size(Y,2),1);
+        X = P*M +repmat(ms,size(Y,2),1);
     otherwise        %% Caso nem um argumento tenha sido passado retorna se 0.
         x = 0;
     endswitch
